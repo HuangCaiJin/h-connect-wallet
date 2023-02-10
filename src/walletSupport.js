48349cb70a4f8910470bcb4c0d00b3aa8f94a675
huangcaijin <2856333717@qq.com>
2023-02-10 15:14:39 +0800

    图标

--- src/walletSupport.js
+++ src/walletSupport.js
@@ -19,7 +19,7 @@ module.exports = [
     },
     {
         "name": "WalletConnect",
-        "icon": "https://oss.nftup.vip/market/2022/09/19/7a6ddd2a4fc94355b1b45df7291ee1fd.png",
+        "icon": "/source/png/walletConnect.png",
         "supportPc": true,
         "pluginObj": "",
         "supportNetwork":[
